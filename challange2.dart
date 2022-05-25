995551fcfc8ea356d02d8a53686454483fd6b0ef
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-25 12:20:28 +0700

    challange 2

---
+++ challange2.dart
@@ -0,0 +1,27 @@
+void main(){
+/*
+    Challenge 2
+1. Buatlah sebuah formula untuk menghitung keliling persegi
+2. Buatlah sebuah formula untuk menghitung luas Lingkaran
+3. Buatlah sebuah formula untuk menghitung volume balok
+*/
+
+// No 1.
+int sisi = 10;
+int keliling = sisi * 4;
+print("Keliling Persegi: $keliling");
+
+// No 2.
+int jari = 10;
+double luasLingkaran = 3.14 * jari * jari;
+print("Luas Lingkaran: $luasLingkaran");
+
+// No 3.
+int panjang = 6;
+int lebar = 8;
+int tinggi = 10;
+int volumeBalok = panjang * lebar * tinggi;
+print("Volume Balok: $volumeBalok");
+
+
+}
