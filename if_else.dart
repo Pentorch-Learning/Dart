cb3b0364c5cf10ecac499e58f515c5c4fbcddc42
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-25 12:42:44 +0700

    if else

---
+++ if_else.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Decision Making
+/*
+if(boolean expression){
+ // dieksekusi apabila boolean expression bernilai true
+}else{
+ // dieksekusi apabila boolean expression bernilai false
+ */
+
+  int angka = 6;
+if(angka == 0){
+  print("Nol");
+}else{
+    if (angka % 2 == 0) {
+    print("Angka $angka adalah bilangan genap");
+  } else {
+    print("Angka $angka adalah bilangan ganjil");
+  }
+}
+
+  String nama = "Lern";
+  if (nama == "Willy") {
+    print("Ini $nama");
+  } else {
+    print("Ini Bukan $nama");
+  }
+}
