8ccd7fcbd5c1a2b3dbd63d9ce8c5c24e716e1c57
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-25 20:01:40 +0700

    do while

---
+++ do_while.dart
@@ -0,0 +1,17 @@
+void main(){
+  /*
+  //do {
+  //  // statements yang akan dijalankan selama condition bernilai true
+  // } while (condition);
+   */
+  int i = 1;
+  do {
+    if (i % 2 == 0) {
+      print(i);
+    }
+    i++;
+  } while (i <= 100);
+
+  // whike li <= 100 {
+  // print(i);}
+}
