95efaced336fc12d40fb14712fa65a459a7a7157
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-25 09:47:30 +0700

    string advanced

--- string.dart
+++ string.dart
@@ -19,5 +19,34 @@ void main() {
 
 // mengubah menjadi list
   print(daftarhewan.split(', ')[1]);
+
+// menampilkan panjang string
+  print(name.length);
+
+  // menghilangkan spasi di depan dan belakang
+  print(name.trim());
+
+  // menghilangkan spasi di depan
+  print(name.trimLeft());
+
+  // menghilangkan spasi di belakang
+  print(name.trimRight());
+  
+
+  //mendapatkan nilai desimal ASCII
+  print(name.codeUnitAt(1));
+
+  //menampilkan index karakter dalam string
+  print(name.indexOf('i'));
+
+  //mengecek apakah diawali dengan string karakter tertentu
+  print(name.startsWith('Willy'));
+
+  //cek apakah string tersebut kosong
+  var kosong = '';
+  print(kosong.isEmpty);
+
+  //cek apakah string tersebut tidak kosong
+  print(kosong.isNotEmpty);
 }
 
