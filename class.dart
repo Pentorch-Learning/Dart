6319db3fb3fea52fcabcd82a852de2aa2aae1ac2
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-26 07:59:45 +0700

    class constructor

--- class.dart
+++ class.dart
@@ -21,22 +21,32 @@ Methods
  */
 
 void main() {
-  RekeningBank rekeningBank = new RekeningBank();
-  rekeningBank.namaPemilik= "Willy";
-  rekeningBank.namaBank = "Bank Mandiri";
-  rekeningBank.saldo = 1000000;
-  print(rekeningBank.namaPemilik);
-  print(rekeningBank.namaBank);
-  print(rekeningBank.saldo);
+  RekeningBank rekeningWilly =
+      new RekeningBank(namaPemilik: "Willy", namaBank: "BNI", saldo: 1000000);
+  rekeningWilly.namaPemilik = "Willy";
+  rekeningWilly.namaBank = "Bank Mandiri";
+  rekeningWilly.saldo = 1000000;
+  print(rekeningWilly.namaPemilik);
+  print(rekeningWilly.namaBank);
+  print(rekeningWilly.saldo);
+  rekeningWilly.cekSaldo();
 
-  rekeningBank.cekSaldo();
+  RekeningBank rekeningRicky =
+      new RekeningBank(namaPemilik: "Ricky", namaBank: "BRI", saldo: 5000000);
+  print(rekeningRicky.saldo);
+  rekeningRicky.cekSaldo();
 }
 
 class RekeningBank {
-   late String namaPemilik;
-   late String namaBank;
-   late int saldo;
+  late String namaPemilik;
+  late String namaBank;
+  late int saldo;
 
+  RekeningBank({String? namaBank, String? namaPemilik, required int saldo}) {
+    this.namaPemilik = namaPemilik!;
+    this.namaBank = namaBank!;
+    this.saldo = saldo;
+  }
 
   cekSaldo() {
     print("Saldo anda sekarang adalah : $saldo");
